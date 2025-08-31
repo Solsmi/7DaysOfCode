5d4c966e7f55f3bfe82604477905283aedeb5b66
Solange San Martin Ibañez <solange.sanmartin.i@gmail.com>
2025-08-31 15:13:44 -0400

    Modificar cometario con solucion dia 2

--- dia2/app.js
+++ dia2/app.js
@@ -1,8 +1,16 @@
-//Forma simple de declarar variables
+//Solucion correcta de declarar variables
 //const nombre = prompt('¿Cuál es tu nombre?');
 //const edad = prompt('¿Cuántos años tienes?');
 //const lenguaje = prompt('¿Qué lenguaje de programación estás estudiando?');
-//alert(`Hola ${nombre}, tienes ${edad} años y ya estás aprendiendo ${lenguaje}!`);
+//const mensaje = "Hola ${nombre}, tienes ${edad} años y ya estás aprendiendo ${lenguaje}!";
+//alert(mensaje);
+//const gusta = prompt("¿Te gusta estudiar ${lenguaje}? Responde con el número 1 - SÍ o 2 - NO");
+//if (gusta == 1){
+//alert("¡Muy bien! Sigue estudiando y tendrás mucho éxito.");
+//}
+//if (gusta == 2){
+//alert("Oh, qué pena... ¿Has intentado aprender otros lenguajes?");
+//}
 
 document.addEventListener('DOMContentLoaded', function() {
     const chatContainer = document.getElementById('chatContainer');
